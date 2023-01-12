c066e4d2361dd99e8b80ede9cab563b1346e558e
Lorenzo Barretta <lorenzo.barretta04@gmail.com>
2023-01-12 09:26:42 +0100

    Fixed indentation.

--- index.php
+++ index.php
@@ -18,7 +18,7 @@
 
         // Check connection
         if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+            die("Connection failed: " . $conn->connect_error);
         }
         echo "Connected successfully";
     ?>
